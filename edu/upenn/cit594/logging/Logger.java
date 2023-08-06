985492474de3ed5101340ede6f300cfc483e2459
etherdd <69791866+etherdd@users.noreply.github.com>
2023-08-06 23:47:54 +0200

    Update Logger.java

--- edu/upenn/cit594/logging/Logger.java
+++ edu/upenn/cit594/logging/Logger.java
@@ -1,7 +1,56 @@
-package edu.upenn.cit594.studenttests.logging;
+package edu.upenn.cit594.logging;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Logger {
+
     private static Logger instance;
-    // Add constructor, instance methods, and properties for logging events
-    // Implement the Singleton pattern
+    private PrintStream output;
+    private boolean isFileOutput;
+    private Logger() {
+        this.output = System.err; // Default output to System.err
+        this.isFileOutput = false;
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void logEvent(String message) {
+        String logMessage = getCurrentTimestamp() + " " + message;
+        output.println(logMessage);
+    }
+
+    public void setOutputDestination(String fileName) {
+        if (isFileOutput) {
+            closeLogFile();
+        }
+        try {
+            this.output = new PrintStream(new FileOutputStream(fileName, true));
+            this.isFileOutput = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeLogFile() {
+        if (output != null && isFileOutput) {
+            output.close();
+            isFileOutput = false;
+        }
+    }
+
+    private String getCurrentTimestamp() {
+        long currentTime = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(currentTime);
+        return sdf.format(date);
+    }
 }
